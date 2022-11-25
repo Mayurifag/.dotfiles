5f13eee33ea6ee0991f130360624ba7a38ad99e1
Vladislav Ponomarev <farazeus@gmail.com>
2022-11-26 00:46:07 +0400

    Remove code settings (sync already done via internal things)

--- makefiles/packages.mk
+++ makefiles/packages.mk
@@ -1,11 +1,8 @@
-.PHONY: packages brew-packages node-packages rust-packages code-settings ruby-packages fzf antibody
-packages: brew-packages node-packages rust-packages code-settings ruby-packages fzf
+.PHONY: packages brew-packages node-packages rust-packages ruby-packages fzf antibody
+packages: brew-packages node-packages rust-packages ruby-packages fzf
 
 brew-packages:
 	brew bundle --file=$(DOTFILES_DIR)/install/Brewfile || true
-
-code-settings:
-	for EXT in $$(cat install/Codefile); do code --install-extension $$EXT; done
 
 node-packages:
 	npm install -g $(shell cat install/npmfile)
