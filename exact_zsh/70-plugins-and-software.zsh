298bbb98a5930610f9653a487f9c932a4acec08b
Mayurifag <Mayurifag@users.noreply.github.com>
2025-09-08 12:55:22 +0400

    refactor(zsh): consolidate brew and mise initialization
    Moved Homebrew and Mise initialization logic into a new `_evalcache` function to improve performance and reduce redundant sourcing. This change also simplifies the plugin loading process and removes an unnecessary file.
    
    - Removed exact_zsh/00-managers.zsh.
    - Added `_evalcache` function to exact_zsh/70-plugins-and-software.zsh for caching command evaluations.
    - Integrated Homebrew shellenv initialization using `_evalcache` in exact_zsh/70-plugins-and-software.zsh.
    - Integrated Mise activation using `_evalcache` in exact_zsh/70-plugins-and-software.zsh.
    - Updated zoxide initialization to use `_evalcache` in exact_zsh/70-plugins-and-software.zsh.
    - Removed `wintermi/zsh-mise` from plugins.txt.
    - Added `mroth/evalcache` and `chrisgrieser/zsh-magic-dashboard` to plugins.txt.
    - Added `ayugram` cask to install/Brewfile.

--- exact_zsh/70-plugins-and-software.zsh
+++ exact_zsh/70-plugins-and-software.zsh
@@ -1,7 +1,14 @@
 safe_source $HOME/zsh/.zsh_plugins.sh
+
+[[ -f /opt/homebrew/bin/brew ]] && _evalcache /opt/homebrew/bin/brew shellenv
+
+safe_source "${HOMEBREW_PREFIX:-}"/opt/antidote/share/antidote/antidote.zsh
+safe_source /usr/share/zsh-antidote/antidote.zsh
 safe_source $HOME/.orbstack/shell/init.zsh
 safe_source $HOME/.fzf.zsh
 
+_evalcache mise activate zsh
+
 if if_command_exists zoxide; then
-  eval "$(zoxide init zsh)"
+  _evalcache zoxide init zsh
 fi
